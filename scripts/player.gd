c9fdb6bf1e24944ccd3c69cb75f43a95602affb4
Chaotic-Legend <gamemasterx40@gmail.com>
2026-05-01 16:30:30 -0400

    Recreating The World's Hardest Game in Godot.

---
+++ scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+class_name Player
+
+var my_variable = Vector2(10, 8)
+var speed = 300.0
+var spawn_point = Vector2(60, 360)
+
+func _physics_process(_delta: float) -> void:
+	var direction = Vector2(0,0)
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x = 1
+	if Input.is_action_pressed("move_left"):
+		direction.x = -1
+	if Input.is_action_pressed("move_up"):
+		direction.y = -1
+	if Input.is_action_pressed("move_down"):
+		direction.y = 1
+	
+	direction = direction.normalized()
+	
+	velocity = direction * speed
+	move_and_slide()
+
+func reset_player():
+	global_position = spawn_point
+	$HitSound.play()
